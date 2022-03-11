eec465676053a69690de701f6030ba07ce7488fa
sai101092 <101318099+sai101092@users.noreply.github.com>
2022-03-11 22:09:22 +0530

    Update 1sample.tf

--- 1sample.tf
+++ 1sample.tf
@@ -1,14 +1,13 @@
-resource "google_service_account" "default" {
-  account_id   = "service_account_id"
-  display_name = "Service Account"
+provider "google" {
+credentials = "${file("gcpcredentials.json")}"
+project_id : "primeval-pen-343614",
+region = "us-central1"
 }
 
 resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
+  name         = "virtual-machine-from-terraform"
+  machine_type = "f1-micro"
   zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
@@ -16,28 +15,34 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
   network_interface {
     network = "default"
 
     access_config {
-      // Ephemeral public IP
+      // Include this section to give the VM an external ip address
     }
   }
 
-  metadata = {
-    foo = "bar"
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+    // Apply the firewall rule to allow external IPs to access this instance
+    tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http-terraform"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
 
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
