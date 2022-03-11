0bddb59fbd6062d0a94a86515838efc330f879da
sai101092 <101318099+sai101092@users.noreply.github.com>
2022-03-11 22:24:06 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,4 +1,5 @@
-provider "google" {
+terraform{
+  provider "google" {
 credentials = "${file("gcpcredentials.json")}"
 project_id : "my-kubernetes-cluster-343811",
 region = "us-central1"
@@ -46,3 +47,5 @@ resource "google_compute_firewall" "http-server" {
 output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
+}
+  
